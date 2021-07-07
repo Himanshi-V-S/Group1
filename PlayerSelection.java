c3bb4a116dd5623a4066c065da7c33e4b0f8ab8f
Himanshi-V-S <himanshisenarath@gmail.com>
2021-07-07 16:00:16 +0530

    Update PlayerSelection.java

--- PlayerSelection.java
+++ PlayerSelection.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
+import import java.util.ArrayList;
+
 public class PlayerSelection {
     public static void main(String [] args){
+		ArrayList<String> players = new ArrayList<String>();
+        
+        players.add("Kusal Perera");
+        players.add("Danushka Gunathilaka");
+        players.add("Kusal Mendis");
+        players.add("Wanindu De Silva");
+        players.add("Lakshan Sandakan");
+        players.add("Dushmantha Chameera");
+        players.add("Lasith Malinga");
+        players.add("Isuru Udana");
         Scanner input = new Scanner(System.in);
 
         String menuChoice = "S";
