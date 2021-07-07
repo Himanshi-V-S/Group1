0a3aa73ba958c9abe6a6b5dd545980f14c02d50d
sachz1997 <87040899+sachz1997@users.noreply.github.com>
2021-07-07 15:30:32 +0530

    Add files via upload

--- PlayerSelection.java
+++ PlayerSelection.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class PlayerSelection {
-	public static void main(String [] args){
-		
-	}
+    public static void main(String[] args) {
+        ArrayList<String> players = new ArrayList<String>();
+        
+        players.add("Kusal Perera");
+        players.add("Danushka Gunathilaka");
+        players.add("Kusal Mendis");
+        players.add("Wanindu De Silva");
+        players.add("Lakshan Sandakan");
+        players.add("Dushmantha Chameera");
+        players.add("Lasith Malinga");
+        players.add("Isuru Udana");
+    }
 }
